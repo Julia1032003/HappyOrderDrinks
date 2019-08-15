5ffec1536ff138673f4e549a6b2b6bd2e8f55ef4
Julia <julia1032003@gmail.com>
2019-08-16 04:28:25 +0800

    Initial Commit

---
+++ HappyOrderDrinks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrderDrinks
+//
+//  Created by Julia Wang on 2019/8/16.
+//  Copyright © 2019 Julia Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
