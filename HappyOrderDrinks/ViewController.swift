fdd64103f884b0e3e7ed60fe5392dad630b652e6
Julia <julia1032003@gmail.com>
2019-08-29 16:25:42 +0800

    新增了店家地圖及打電話給店家功能

--- HappyOrderDrinks/ViewController.swift
+++ HappyOrderDrinks/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var drinksStoreMKView: MKMapView!
+    
+   
+    func showDrinksStorePoint(){
+        let location = CLLocation(latitude: 25.051138, longitude: 121.534933)
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 300, longitudinalMeters: 300)
+            drinksStoreMKView.setRegion(region, animated: true)
+        let annotation = MyAnnotation(coordingnate: location.coordinate)
+        annotation.title = "可不可熟成紅茶"
+        annotation.subtitle = "台北伊通店"
+        drinksStoreMKView.addAnnotation(annotation)
+        
+        
+    }
+    
+    @IBAction func callActionButton(_ sender: Any) {
+        let controller = UIAlertController(title: "可不可熟成紅茶-台北伊通店", message: "確定要打電話嗎？", preferredStyle: .actionSheet)
+        let phoneNumber = "0225175510"
+        let phoneAction = UIAlertAction(title: "打電話給\(phoneNumber)" , style: .default){(_) in
+            if let url = URL(string: "tel:\(phoneNumber)"){
+                if UIApplication.shared.canOpenURL(url){
+                    UIApplication.shared.open(url,options: [:], completionHandler: nil)
+                }else{
+                    print("無法開啟URL")
+                }
+            }else{
+                print("連結錯誤")
+            }
+            print("error")
+        }
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        controller.addAction(phoneAction)
+        controller.addAction(cancelAction)
+        present(controller,animated: true ,completion: nil)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        showDrinksStorePoint()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+       
+    
 }
 
